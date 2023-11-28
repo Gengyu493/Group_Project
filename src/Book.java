639e679c25a0ebaaffc97c0a2e5e8e67e4403bdb
jahlilniles <jahliln1@umbc.edu>
2023-11-27 19:03:25 -0500

    name

---
+++ src/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+    private String title;
+    private  String author;
+
+    // Using a constructor
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+
+    }
+
+}
---
+++ src/MainLibrarySystem.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class MainLibrarySystem {
+    public static final int MAX_BOOKS = 500;
+    private static int amountOfBooks = 0;
+    private static Book[] library = new Book[MAX_BOOKS];
+
+}
